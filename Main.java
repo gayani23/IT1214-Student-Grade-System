b12c3fd1a65d21437edce86ca5255909ada113fe
G.P Udari <pudari446@gmail.com>
2026-06-07 19:15:23 +0530

    Initial project setup

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- Student Grade Management System ---");
+        System.out.println("1. Add a Student");
+        System.out.println("2. Display All Students");
+        System.out.println("3. Search for a Student by ID");
+        System.out.println("4. Calculate and Display Average Mark");
+        System.out.println("5. Exit");
+    }
+}
